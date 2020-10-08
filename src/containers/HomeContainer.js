472199bcbd36da9fc9c0f1fa8da8d0de20a70c2b
Rizwan Ali <rizwanali1055@gmail.com>
2020-10-08 17:44:22 -0400

    Make container

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,11 @@
+import {connect} from 'react-redux';
+import {addToCart} from '../services/actions/actions';
 import Home from '../components/Home';
 
-export default Home;
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({
+  addToCartHandler: data => dispatch(addToCart)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
