932f6e207b8a83b14b2c44bceff4553fe85b7cb0
Rizwan Ali <rizwanali1055@gmail.com>
2020-10-10 03:36:51 -0400

    useState functional component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import HomeContainer from './containers/HomeContainer';
 import Person from './components/Person/Person';
 import { Button } from '@material-ui/core'
 
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    persons: [
-     { name: 'Max', age: 27 },
-     { name: 'Manu', age: 24 }
-    ]
+
+  const [persons, setPersons] = useState([
+    { name: 'Max', age: 27 },
+    { name: 'Manu', age: 24 }
+  ])
+
+  const switchNameHandler = () => {
+    setPersons([
+      { name: "Maxmillian", age: 27 },
+      { name: 'Manu', age: 20 }
+    ])
   }
 
-  switchNameHandler = () => {
-    this.setState({
-      persons: [
-       { name: "Maxmillian", age: 27 },
-       { name: 'Manu', age: 20 }
-      ]
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>App Component</h1>
-        <HomeContainer />
-        <div style={{marginTop: '15px'}}>
-          <Button variant="outlined" color="primary" onClick={this.switchNameHandler}>Switch name </Button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My hobbies are: Racing </Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        </div>
+  return (
+    <div className="App">
+      <h1>App Component</h1>
+      <HomeContainer />
+      <div style={{marginTop: '15px'}}>
+        <Button variant="outlined" color="primary" onClick={switchNameHandler}>Switch name </Button>
+        <Person name={persons[0].name} age={persons[0].age}> My hobbies are: Racing </Person>
+        <Person name={persons[1].name} age={persons[1].age} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default (App);
+export default App;
